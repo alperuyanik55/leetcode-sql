3ed20dd5e6b119e69566d81da3f293007311b2d8
alperuyanik55 <alperuyan1k5534@gmail.com>
2025-03-19 01:11:36 +0300

    Update percentage-of-users-attended-a-contest.sql

--- LAB-07-sql/Basic Aggregate Functions/percentage-of-users-attended-a-contest.sql
+++ LAB-07-sql/Basic Aggregate Functions/percentage-of-users-attended-a-contest.sql
@@ -32,4 +32,6 @@ SELECT  COUNT(user_id) -- Total number of unique users
 FROM  Users   ), 2 ) AS percentage -- The percentage of users registered for each contest, rounded to 2 decimal places
 FROM  Register -- The table containing registration information
 GROUP BY  contest_id -- Group the data by contest ID
-ORDER BY  percentage DESC, -- Order the results by percentage in descending order contest_id; -- Then order by contest ID for ties
+ORDER BY 
+  percentage DESC, -- Order the results by percentage in descending order
+  contest_id; -- Then order by contest ID for ties
