7dbca7f66d5289393c25a1461477c7dcc2c1d74d
alperuyanik55 <alperuyan1k5534@gmail.com>
2025-03-19 01:17:56 +0300

    Update queries-quality-and-percentage.sql

--- LAB-07-sql/Basic Aggregate Functions/queries-quality-and-percentage.sql
+++ LAB-07-sql/Basic Aggregate Functions/queries-quality-and-percentage.sql
@@ -30,12 +30,5 @@ Return the result table in any order.
 
 The result format is in the following example.
 */
-WITH QueryStats AS ( SELECT  query_name,
-AVG(CAST(rating AS FLOAT) / position) AS quality,100.0 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) AS
-poor_query_percentage
-FROM Queries
-GROUP BY query_name)
-SELECT  query_name, 
-ROUND(quality, 2) AS quality, 
-ROUND(poor_query_percentage, 2) AS poor_query_percentage
-FROM QueryStats;
+
+???
