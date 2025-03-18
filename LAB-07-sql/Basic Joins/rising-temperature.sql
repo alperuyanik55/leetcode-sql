701f5a29eec6ce20254e8deef6d5317d5293ed86
alperuyanik55 <alperuyan1k5534@gmail.com>
2025-03-19 00:32:01 +0300

    Update rising-temperature.sql

--- LAB-07-sql/Basic Joins/rising-temperature.sql
+++ LAB-07-sql/Basic Joins/rising-temperature.sql
@@ -1,5 +1,5 @@
 /*
-    Question url: https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+    Question url: https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
     Question: 
    +---------------+---------+
 | Column Name   | Type    |
